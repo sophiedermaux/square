bc12abee779c94e1a427332852f0116b15fe1fa9
Sophie Dermaux <sophiebcdermaux@gmail.com>
2018-03-20 12:47:16 +0000

    made changes to specs to compare array sqs

--- Documents/Projects/square_creator/square_creator.rb
+++ Documents/Projects/square_creator/square_creator.rb
@@ -6,28 +6,21 @@ class SquareCreator
     @height = height.to_i
   end
 
-# method to generate the SquareCreator class when answer given
-  def generate
-    create_array.each do |row|
-      puts row.join
-    end
-  end
-
-private
-
-# when generate method called above it generates a new array based on height and width
-  def create_array
-    original_array = (0..@height).map {Array.new(@width," ")}
+  # when generate method called above it generates a new array based on height and width
+  def create_square_array
+    original_array = (0...@height).map { Array.new(@width," ") }
     in_ascii_characters(original_array)
   end
 
-# method that says that the character goes based on the position in the row
+  private
+
+  # method that says that the character goes based on the position in the row
   def inject_character(row, character, position)
     row[position] = character
     row
   end
 
-# define the order of the characters for each of the rows based on first, middle and last rows
+  # define the order of the characters for each of the rows based on first, middle and last rows
   def first_row(row)
     inject_character(row, '┌', 0)
     (1..row.length-1).each do |c|
@@ -51,19 +44,20 @@ private
     row
   end
 
-  def in_ascii_characters(array, acc = [])
+  def in_ascii_characters(array, acc = [], counter = 0)
     if array.empty?
       acc
     else
       row = array.shift
-      if acc.empty?
+      if counter == 0
         acc << first_row(row)
-      elsif array.empty?
+      elsif counter == @height-1
         acc << last_row(row)
       else
         acc << middle_row(row)
       end
-    in_ascii_characters(array, acc)
+
+      in_ascii_characters(array, acc, counter + 1)
     end
   end
 end
@@ -73,7 +67,11 @@ cli = HighLine.new
 answer = cli.ask "Please say your desired width and height seperated by a space e.g 5 5"
 width, height = answer.split
 
-SquareCreator.new(width,height).generate
+array = SquareCreator.new(width,height).create_square_array
+
+array.each do |row|
+  puts row.join
+end
 
 # RSpec tests
 
@@ -81,16 +79,172 @@ require 'rspec/autorun'
 
 RSpec.describe SquareCreator, :type => :model do
   describe "square generation" do
-    it "checks if different widths and heights create a sq" do
-      expect(SquareCreator.new(10,10).generate).to eq(SquareCreator.new(10,10).generate)
-      expect(SquareCreator.new(2,2).generate).to eq(SquareCreator.new(2,2).generate)
-      expect(SquareCreator.new(10,5).generate).to eq(SquareCreator.new(10,5).generate)
-      expect(SquareCreator.new(5,10).generate).to eq(SquareCreator.new(5,10).generate)
-      expect(SquareCreator.new(4,6).generate).to eq(SquareCreator.new(4,6).generate)
-      expect(SquareCreator.new(3,10).generate).to eq(SquareCreator.new(3,10).generate)
-      expect(SquareCreator.new(5,5).generate).to eq(SquareCreator.new(5,5).generate)
-      expect(SquareCreator.new(4,8).generate).to eq(SquareCreator.new(4,8).generate)
-      expect(SquareCreator.new(3,9).generate).to eq(SquareCreator.new(3,9).generate)
+
+    it "returns a 10 x 10 ascii array" do
+      expected = [
+        ["┌", "-", "-", "-", "-", "-", "-", "-", "-", "┐"],
+        ["|", " ", " ", " ", " ", " ", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", " ", " ", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", " ", " ", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", " ", " ", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", " ", " ", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", " ", " ", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", " ", " ", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", " ", " ", " ", " ", " ", "|"],
+        ["└", "-", "-", "-", "-", "-", "-", "-", "-", "┘"]
+      ]
+
+      expect(SquareCreator.new(10,10).create_square_array).to eq(expected)
+    end
+
+    it "returns a 5 x 5 ascii array" do
+      expected = [
+        ["┌", "-", "-", "-", "┐"],
+        ["|", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", "|"],
+        ["└", "-", "-", "-", "┘"]
+      ]
+
+      expect(SquareCreator.new(5,5).create_square_array).to eq(expected)
+    end
+
+    it "returns a 4 x 5 ascii array" do
+      expected = [
+        ["┌", "-", "-", "┐"],
+        ["|", " ", " ", "|"],
+        ["|", " ", " ", "|"],
+        ["|", " ", " ", "|"],
+        ["└", "-", "-", "┘"]
+      ]
+
+      expect(SquareCreator.new(4,5).create_square_array).to eq(expected)
+    end
+
+    it "returns a 5 x 6 ascii array" do
+      expected = [
+        ["┌", "-", "-", "-", "┐"],
+        ["|", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", "|"],
+        ["|", " ", " ", " ", "|"],
+        ["└", "-", "-", "-", "┘"]
+      ]
+
+      expect(SquareCreator.new(5,6).create_square_array).to eq(expected)
+    end
+
+    it "returns a 3 x 3 square of arrays" do
+      expected = [
+        ["┌", "-", "┐"],
+        ["|", " ", "|"],
+        ["└", "-", "┘"]
+      ]
+
+      expect(SquareCreator.new(3,3).create_square_array).to eq(expected)
+    end
+
+    it "returns a 10 x 10 square of arrays" do
+      expected = [
+        ["┌", "-", "-", "-","-","-","-","-","-", "┐"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["└", "-", "-", "-","-","-","-","-","-", "┘"]
+      ]
+
+      expect(SquareCreator.new(10,10).create_square_array).to eq(expected)
+    end
+
+    it "returns a 10 x 13 square of arrays" do
+      expected = [
+        ["┌", "-", "-", "-","-","-","-","-","-", "┐"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["└", "-", "-", "-","-","-","-","-","-", "┘"]
+      ]
+
+      expect(SquareCreator.new(10,13).create_square_array).to eq(expected)
+    end
+
+    it "returns a 10 x 15 square of arrays" do
+      expected = [
+        ["┌", "-", "-", "-","-","-","-","-","-", "┐"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," ", "|"],
+        ["└", "-", "-", "-","-","-","-","-","-", "┘"]
+      ]
+
+      expect(SquareCreator.new(10,15).create_square_array).to eq(expected)
+    end
+
+    it "returns a 15 x 15 square of arrays" do
+      expected = [
+        ["┌", "-", "-", "-","-","-","-"," -","-","-","-","-","-","-", "┐"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," "  "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," "  "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["└", "-", "-", "-","-","-","-","-","-","-","-","-","-","-", "┘"]
+      ]
+
+      expect(SquareCreator.new(15,15).create_square_array).to eq(expected)
+    end
+
+    it "returns a 15 x 5 square of arrays" do
+      expected = [
+        ["┌", "-", "-", "-","-","-","-","-","-","-","-","-","-","-", "┐"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["└", "-", "-", "-","-","-","-","-","-","-","-","-","-","-", "┘"]
+      ]
+
+      expect(SquareCreator.new(15,5).create_square_array).to eq(expected)
+    end
+
+    it "returns a 15 x 3 square of arrays" do
+      expected = [
+        ["┌", "-", "-", "-","-","-","-","-","-","-","-","-","-","-", "┐"],
+        ["|", " ", " ", " "," "," "," "," "," "," "," "," "," "," ", "|"],
+        ["└", "-", "-", "-","-","-","-","-","-","-","-","-","-","-", "┘"]
+      ]
+
+      expect(SquareCreator.new(15,3).create_square_array).to eq(expected)
     end
   end
 end
